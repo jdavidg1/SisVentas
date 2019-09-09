1482a6dccf365bb7cf07c6380721893b3567657c
jdavidg1 <jdavidg_1@hotmail.com>
2019-09-08 21:51:48 -0500

    Creacion de la clase Conexion y código de conexión con la base de datos.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+
+        public static string Cn = "Data Source= LAPTOP-9VN6QT9R; Initial Catalog=dbventas; Integrated Security=true";   //Declarar la cadena de conexión, Cn, de cadana de conexión, es una variable pública. 
+                                                                                                                        //https://www.youtube.com/watch?v=Qhx4Wd9W_iA&list=PLyV0e_0UKpqs1xL3h50A-sEwslA9cDNQD&index=11
+                                                                                                                        //El servidor de datos es donde está la base de datos y se puede ver en SQL el nombre del equipo o IP, Initial Catalog es el nombre de la base de datos
+                                                                                                                        //Integrated Security es el tipo de seguridad que se va a emplear.
+
+
+    }
+}
